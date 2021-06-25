64008bb706b90f82c6d09a2d75f301ed2e529640
phongnm-dev <p001842@PC-001842-1.local>
2021-06-25 15:51:26 +0700

    Updates

--- js/google-tag-manager.js
+++ js/google-tag-manager.js
@@ -3,15 +3,3 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WC9P7DQ');
-
-// (function() {
-//   var noscript = document.createElement('noscript');
-//   var iframe = document.createElement('iframe');
-//   iframe.setAttribute('height', "0")
-//   iframe.setAttribute('width', "0")
-//   iframe.src = "https://www.googletagmanager.com/ns.html?id=GTM-WC9P7DQ";
-//   iframe.style.display = "none";
-//   iframe.style.visibility = "hidden";
-//   noscript.appendChild(iframe);
-//   document.body.prepend(noscript);
-// })();
